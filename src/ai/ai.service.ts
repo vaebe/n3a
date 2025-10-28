fb5da60a69bb445d328208897d8e7e8e57796fd4
vae <18137693952@163.com>
2025-10-28 17:31:16 +0800

    feat(ai): langchain 调用 ai 实现建议 agent

--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { ChatOpenAI } from '@langchain/openai';
+import { createAgent, tool } from 'langchain';
+import * as z from 'zod';
+
+const getWeather = tool((input) => `It's always sunny in ${input.city}!`, {
+  name: 'get_weather',
+  description: 'Get the weather for a given city',
+  schema: z.object({
+    city: z.string().describe('The city to get the weather for'),
+  }),
+});
 
 @Injectable()
 export class AiService {
-  chat(id: string) {
-    return `这是一个 ai chat ${id}`;
+  async chat(id: string) {
+    const model = new ChatOpenAI({
+      model: 'deepseek-v3.2',
+      apiKey: process.env.OPENAI_API_KEY,
+      configuration: {
+        baseURL: 'https://apis.iflow.cn/v1',
+      },
+      temperature: 1.3,
+    });
+
+    const agent = createAgent({
+      model,
+      tools: [getWeather],
+    });
+
+    console.log(id);
+
+    const msg = await agent.invoke({
+      messages: [{ role: 'user', content: "What's the weather in Tokyo?" }],
+    });
+
+    return msg;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AiModule } from './ai/ai.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [AiModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        OPENAI_API_KEY: Joi.string().required(),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+      }),
+      validationOptions: {
+        allowUnknown: true, // 控制是否允许环境变量中未知的键
+        abortEarly: true, // true，在遇到第一个错误时就停止验证；false，返回所有错误。默认为false。
+      },
+    }),
+    AiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
