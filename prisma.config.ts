80921b33adc9d74918e85ebac7622d5a80fa050d
vae <18137693952@163.com>
2026-01-07 10:39:21 +0800

    feat: prisma 目录移动到 src 中

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,9 +2,9 @@ import 'dotenv/config';
 import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: 'src/prisma/schema.prisma',
   migrations: {
-    path: 'prisma/migrations',
+    path: 'src/prisma/migrations',
   },
   datasource: {
     url: env('PRISMA_DB'),
