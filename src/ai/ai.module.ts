089d3c56104146484c1c9388f8d1d6cccfa27587
vae <18137693952@163.com>
2026-01-07 13:06:58 +0800

    feat(ai): service 增加 createAiMessage 函数

--- src/ai/ai.module.ts
+++ src/ai/ai.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AiController } from './ai.controller';
 import { AiService } from './ai.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [AiController],
-  providers: [AiService],
+  providers: [AiService, PrismaService],
 })
 export class AiModule {}
--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AiMessage } from '../generated/prisma/client';
+import { UIMessage } from 'ai';
+
+interface CreateAiMessageProps {
+  message: UIMessage;
+  chatId: string;
+  userId: string;
+}
 
 @Injectable()
-export class AiService {}
+export class AiService {
+  constructor(private prisma: PrismaService) {}
+
+  createAiMessage(props: CreateAiMessageProps): Promise<AiMessage> {
+    const { message, userId, chatId } = props;
+    const { id, role, metadata, parts } = message;
+
+    return this.prisma.aiMessage.create({
+      data: {
+        userId,
+        id,
+        conversationId: chatId,
+        role,
+        metadata: JSON.stringify(metadata),
+        parts: JSON.stringify(parts),
+      },
+    });
+  }
+}
